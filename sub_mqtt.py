90a6951a58200553e1dbafc50938f2af8b04b6f8
root <root@Germanvelardez.peron>
2022-05-03 16:06:57 -0300

    iniciamos proyecto

--- sub_mqtt.py
+++ sub_mqtt.py
@@ -25,6 +25,8 @@ def on_connect(client,userdata,flags,rc):
 
         print("[{}] conectado con el servidor".format(_API_NAME_))
         print("[{}] Publicando mensajes en el topic {}".format(_API_NAME_,TOPIC))
+        client.subscribe(TOPIC)
+
     else:
         print("[{}]Error al conectarse al servidor".format(_API_NAME_))
 
@@ -37,27 +39,28 @@ def on_disconnected(client,userdata,rc):
         print("[{}] desconectado del servidor".format(_API_NAME_))
 
 
+def on_message(client, userdata, msg):
+    print("mensaje recibido:")
+    buffer = str(msg.payload.decode("utf-8"))
+    print("mensaje recibido {}".format(buffer))
+
+
 
 
 
 def Main():
     print(_API_NAME_)
-    client = mqtt.Client(client_id = "simo-config", clean_session= False)
+    client = mqtt.Client(client_id = "simo-test", clean_session= False)
     print("conectado con {} mediante puerto {}".format(URL,PORT))
     client.on_connect = on_connect
     client.on_disconnected = on_disconnected
+    client.on_message = on_message
+
     client.username_pw_set(ID,PASS)
     client.connect(host = URL,port = PORT, keepalive = 60)
+    client.loop_forever()
 
 
-
-    counter = 0 
-
-    while True:
-        print("Envio {} al topico {}".format(counter,TOPIC))
-        client.publish(TOPIC,"contador: {}".format(counter))
-        counter += 1
-        time.sleep(10)
 
 
 
